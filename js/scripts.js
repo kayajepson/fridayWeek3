62f8cb3a7ad290b7fd23a2decd9b93d790f498b9
kaya <jepsonkaya@gmail.com>
2019-04-05 10:43:05 -0700

    add function to convert numver from 1 to beep and consolelog it

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,16 @@ $(document).ready(function() {
   $("form#userInput").submit(function(event) {
     event.preventDefault();
     var userNumber = parseInt($("input#numInput").val());
-
-
-  
-    $("#result").text(output);
+    // var output = genOutput(userNumber);
+    // $("#result").text(output);
 
   });
 });
+
+//BACK END
+function convert(num) {
+if (num.includes('1')) {
+      console.log(" beep");
+      }
+  console.log(" " + num);
+}
