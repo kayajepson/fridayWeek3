06aae717bcb1dd125416ec533ea78bfc8867222b
kaya <jepsonkaya@gmail.com>
2019-04-05 09:39:33 -0700

    add front end logic to gather input from form

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
-//BACK END
-
-
-
-
 //FRONT END
+$(document).ready(function() {
+  $("form#userInput").submit(function(event) {
+    event.preventDefault();
+    var userNumber = parseInt($("input#numInput").val());
+
+
+  
+    $("#result").text(output);
+
+  });
+});
